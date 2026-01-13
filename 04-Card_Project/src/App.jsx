f99f351534058e1b5815ef15cd2c5d1551af568b
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-13 15:23:23 +0530

    Props Project done

--- 04-Card_Project/src/App.jsx
+++ 04-Card_Project/src/App.jsx
@@ -2,10 +2,117 @@ import React from 'react'
 import Card  from './components/Card.jsx'
 
 const App = () => {
+    const jobOpenings = [
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+      companyName: "Google",
+      datePosted: "5 days ago",
+      post: "Software Engineer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$45/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Meta-Logo.png",
+      companyName: "Meta",
+      datePosted: "2 weeks ago",
+      post: "Frontend Developer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$50/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkQZwfIF1UdjlMBtjg4fmlfdGU0suXMgG5qA&s",
+      companyName: "Amazon",
+      datePosted: "10 days ago",
+      post: "Cloud Support Engineer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$40/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+      companyName: "Apple",
+      datePosted: "3 weeks ago",
+      post: "iOS Developer",
+      tag1: "Full Time",
+      tag2: "Senior Level",
+      pay: "$65/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
+      companyName: "Netflix",
+      datePosted: "1 week ago",
+      post: "Backend Engineer",
+      tag1: "Part Time",
+      tag2: "Senior Level",
+      pay: "$70/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+      companyName: "Microsoft",
+      datePosted: "4 days ago",
+      post: "DevOps Engineer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$55/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg",
+      companyName: "IBM",
+      datePosted: "6 days ago",
+      post: "Data Analyst",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$35/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg",
+      companyName: "Oracle",
+      datePosted: "2 days ago",
+      post: "Database Engineer",
+      tag1: "Full Time",
+      tag2: "Senior Level",
+      pay: "$60/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://blog.logomaster.ai/hs-fs/hubfs/intel-logo-3.jpg?width=672&height=448&name=intel-logo-3.jpg",
+      companyName: "Intel",
+      datePosted: "3 days ago",
+      post: "Hardware Engineer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$52/hour",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://cdn.worldvectorlogo.com/logos/adobe-acrobat-dc-logo-2020.svg",
+      companyName: "Adobe",
+      datePosted: "8 days ago",
+      post: "UI/UX Designer",
+      tag1: "Part Time",
+      tag2: "Junior Level",
+      pay: "$38/hour",
+      location: "Mumbai, India"
+    }
+  ];
+
+
   return (
     <div className='parent'>
-      <Card />
-      <Card />
+      {jobOpenings.map(function(elem,idx){
+        return <div key={idx}>
+          <Card logo = {elem.brandLogo} company = {elem.companyName} date = {elem.datePosted} post = {elem.post} tag1 = {elem.tag1} tag2 = {elem.tag2} sal = {elem.pay} location = {elem.location}/>
+        </div>
+      })}
     </div>
   )
 }
--- 04-Card_Project/src/components/Card.jsx
+++ 04-Card_Project/src/components/Card.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import {Bookmark, Car} from 'lucide-react'
 
-const Card = () => {
+const Card = (props) => {
+
+  console.log(props.company)
+
   return (
     <div className="card">
         <div>
           <div className="top">
-          <img src="https://images.icon-icons.com/2699/PNG/512/amazon_logo_icon_169611.png" alt="" />
+          <img src= {props.logo} alt="" />
           <button>Save <Bookmark size={12} /></button>
         </div>
         <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
+          <h3>{props.company} <span>{props.date}</span></h3>
+          <h2>{props.post}</h2>
           <div className="tag">
-            <h4>Part Time</h4>
-            <h4>Sineor Level</h4>
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
         </div>
         </div>
         <div className="bottom">
           <div>
-            <h3>$120/hr</h3>
-            <p>Mumbai, India</p>
+            <h3>{props.sal}</h3>
+            <p>{props.location}</p>
           </div>
           <button>Apply Now</button>
         </div>
